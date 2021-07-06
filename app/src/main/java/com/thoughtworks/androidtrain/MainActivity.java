bb18084b44db5ea328a7937c020b46148cda5867
yunlong.wang <ylwang9627@foxmail.com>
2021-07-06 20:38:24 +0800

    yl #0000 constraint layout

--- app/src/main/java/com/thoughtworks/androidtrain/MainActivity.java
+++ app/src/main/java/com/thoughtworks/androidtrain/MainActivity.java
@@ -2,8 +2,11 @@ package com.thoughtworks.androidtrain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +15,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_frame_main);
+        setContentView(R.layout.activity_main);
 
-        Log.d(TAG,"MainActivity Creater");
+        Log.d(TAG, "MainActivity Creater");
+
+        Button button = (Button) findViewById(R.id.startButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+
+    private void openNewActivity() {
+        Intent intent = new Intent(this, ConstraintActivity.class);
+        startActivity(intent);
     }
 }
