1d46d60a9d986df9b7faaecba3bddedf8815d3a6
yunlong.wang <ylwang9627@foxmail.com>
2021-07-07 23:50:46 +0800

    yl #0000 hide action bar and status bar

--- app/src/main/java/com/thoughtworks/androidtrain/LoginActivity.java
+++ app/src/main/java/com/thoughtworks/androidtrain/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.androidtrain;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getActionBar();
+        if (actionBar!=null) actionBar.hide();
     }
 }
