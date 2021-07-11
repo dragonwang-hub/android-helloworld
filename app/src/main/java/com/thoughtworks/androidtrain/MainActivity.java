5a94a6dc824825ef8d207b9bdd9642f5f9c63c0b
yunlong.wang <ylwang9627@foxmail.com>
2021-07-11 10:45:55 +0800

    yl #0000 implement pick contact and toast on android 7.0

--- app/src/main/java/com/thoughtworks/androidtrain/MainActivity.java
+++ app/src/main/java/com/thoughtworks/androidtrain/MainActivity.java
@@ -2,11 +2,16 @@ package com.thoughtworks.androidtrain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,12 +41,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Button pickContact = (Button) findViewById(R.id.pickContact);
-        pickContact.setOnClickListener(v -> pickContactActivity());
-    }
-
-    private void pickContactActivity() {
-        Intent pickContactActivity = new Intent(this,PickContactActivity.class);
-        startActivity(pickContactActivity);
+        pickContact.setOnClickListener(v -> selectContact());
     }
 
     private void openLoginActivity() {
@@ -52,5 +52,37 @@ public class MainActivity extends AppCompatActivity {
     private void openConstraintActivity() {
         Intent intent = new Intent(this, ConstraintActivity.class);
         startActivity(intent);
+    }
+
+    static final int REQUEST_SELECT_CONTACT = 1;
+
+    @SuppressLint({"IntentReset", "QueryPermissionsNeeded"})
+    private void selectContact() {
+        Intent pickContact = new Intent(Intent.ACTION_PICK);
+        pickContact.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+        if (pickContact.resolveActivity(getPackageManager()) != null) {
+        Log.d(TAG, "Start activity for result.");
+        startActivityForResult(pickContact, REQUEST_SELECT_CONTACT);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "RESULT_CODE:" + resultCode + requestCode);
+        if (requestCode == REQUEST_SELECT_CONTACT && resultCode == RESULT_OK) {
+            String[] params = new String[]{ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER};
+            Cursor cursor = getContentResolver().query(data.getData(), params, null, null, null);
+
+            if (cursor != null && cursor.moveToFirst()){
+                String contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                String phoneNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                cursor.close();
+                Log.d(TAG, contactName + " " + phoneNumber);
+                Toast.makeText(this, contactName + " " + phoneNumber, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Invalid select, Please check it.", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
