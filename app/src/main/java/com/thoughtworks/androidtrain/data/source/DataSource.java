33747b9b576cc71bf2426210f80da1887d00fb34
yunlong.wang <ylwang9627@foxmail.com>
2021-07-25 16:07:16 +0800

    yl #0000 implement AppDataBase & create the DataSource interface

---
+++ app/src/main/java/com/thoughtworks/androidtrain/data/source/DataSource.java
@@ -0,0 +1,11 @@
+package com.thoughtworks.androidtrain.data.source;
+
+import com.thoughtworks.androidtrain.data.model.Tweet;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.core.Flowable;
+
+public interface DataSource {
+    Flowable<List<Tweet>> fetchTweets();
+}
--- app/src/main/java/com/thoughtworks/androidtrain/data/source/local/room/AppDataBase.java
+++ app/src/main/java/com/thoughtworks/androidtrain/data/source/local/room/AppDataBase.java
@@ -3,10 +3,22 @@ package com.thoughtworks.androidtrain.data.source.local.room;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+import com.thoughtworks.androidtrain.data.source.local.room.dao.CommentDao;
+import com.thoughtworks.androidtrain.data.source.local.room.dao.ImageDao;
+import com.thoughtworks.androidtrain.data.source.local.room.dao.SenderDao;
 import com.thoughtworks.androidtrain.data.source.local.room.dao.TweetDao;
+import com.thoughtworks.androidtrain.data.source.local.room.entity.CommentEntity;
+import com.thoughtworks.androidtrain.data.source.local.room.entity.ImageEntity;
+import com.thoughtworks.androidtrain.data.source.local.room.entity.SenderEntity;
 import com.thoughtworks.androidtrain.data.source.local.room.entity.TweetEntity;
 
-@Database(entities = {TweetEntity.class}, version = 1)
+@Database(entities = {TweetEntity.class, SenderEntity.class, ImageEntity.class, CommentEntity.class}, version = 1)
 public abstract class AppDataBase extends RoomDatabase {
     public abstract TweetDao tweetDao();
+
+    public abstract SenderDao senderDao();
+
+    public abstract ImageDao imageDao();
+
+    public abstract CommentDao commentDao();
 }
