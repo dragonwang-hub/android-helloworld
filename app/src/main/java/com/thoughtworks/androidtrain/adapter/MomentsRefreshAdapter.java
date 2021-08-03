fb68a519c389966c50b965d927441620fe5963b6
yunlong.wang <ylwang9627@foxmail.com>
2021-08-03 22:51:13 +0800

    yl #0000 add moment refresh header item & use space view to achieve negative margin

---
+++ app/src/main/java/com/thoughtworks/androidtrain/adapter/MomentsRefreshAdapter.java
@@ -0,0 +1,53 @@
+package com.thoughtworks.androidtrain.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.thoughtworks.androidtrain.R;
+
+import org.jetbrains.annotations.NotNull;
+
+public class MomentsRefreshAdapter  extends RecyclerView.Adapter {
+    @NonNull
+    @NotNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    static class MomentsViewHolder extends RecyclerView.ViewHolder {
+        TextView nickName;
+        TextView content;
+        ImageView avatar;
+
+        public MomentsViewHolder(View viewItem) {
+            super(viewItem);
+            // get component
+            this.nickName = viewItem.findViewById(R.id.nickName);
+            this.content = viewItem.findViewById(R.id.content);
+            this.avatar = viewItem.findViewById(R.id.avatar);
+        }
+    }
+
+    static class HeaderViewHolder extends RecyclerView.ViewHolder {
+
+        public HeaderViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
